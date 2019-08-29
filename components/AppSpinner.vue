e73a8db524bdb8ee1e32541fc62264856cebde01
Inga Balcune <inga.balcune@gmail.com>
2019-08-29 11:39:51 +0200

    changed spinner to work on the IE

--- components/AppSpinner.vue
+++ components/AppSpinner.vue
@@ -10,23 +10,68 @@ export default {
 
 <style lang='scss'>
 
-.spinning {
-  display: inline-block;
-  text-align: center;
-  width: 5rem;
-  height: 5rem;
-  border: .3rem solid rgba($color-primary,.4);
-  border-radius: 50%;
-  border-top-color: $color-primary;
-  animation: spin .8s ease-in-out infinite;
-  -webkit-animation: spin .8s ease-in-out infinite;
+// .spinning {
+//   display: inline-block;
+//   text-align: center;
+//   width: 5rem;
+//   height: 5rem;
+//   border: .3rem solid rgba($color-primary,.4);
+//   border-radius: 50%;
+//   border-top-color: $color-primary;
+//   animation: spin .8s ease-in-out infinite;
+//   -webkit-animation: spin .8s ease-in-out infinite;
+// }
+
+// @keyframes spin {
+//   to { -webkit-transform: rotate(360deg); }
+// }
+// @-webkit-keyframes spin {
+//   to { -webkit-transform: rotate(360deg); }
+// }
+
+
+
+// $spinning-size: 20px;
+// $spinning-color: #dc1928;
+// $spinning-border-size: 2px;
+
+
+
+// spinning
+
+.spinning,
+.spinning:after {
+  // position: relative;
+  box-sizing: border-box;
 }
 
-@keyframes spin {
-  to { -webkit-transform: rotate(360deg); }
+.spinning {
+  width: 5rem;
+  height: 5rem;
+  display: inline-block;
+  // color: $spinning-color;
+  
+  &:after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    border: .3rem solid rgba($color-primary,.4);
+    border-radius: 50%;
+    border-top-color: $color-primary;
+    background: transparent;
+    
+    animation: ball-clip-rotate .8s ease-in-out infinite;
+  }
 }
-@-webkit-keyframes spin {
-  to { -webkit-transform: rotate(360deg); }
+
+@keyframes ball-clip-rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
 </style>
